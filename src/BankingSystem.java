f3684ee53bbd533e82a1a50105feab350022f2f8
Tosimi <tosimiadesoye111@gmail.com>
2023-07-03 10:03:40 +0100

    ad

--- src/BankingSystem.java
+++ src/BankingSystem.java
@@ -4,7 +4,7 @@ public class BankingSystem {
     protected String name;
     protected String id;
 
-    protected Double balance;
+    protected Double balance = 0.0;
     protected Double withdraw;
     protected Double deposit;
     protected Double transaction;
@@ -18,20 +18,24 @@ public class BankingSystem {
         id = UserId;
     }
 
+    protected void accountHolding() {
+        if (balance < 0.0) {
+            System.out.println("You own the bank £" + Math.abs(balance));
+        } else {
+            System.out.println("Your account balance is now £" + balance);
+        }
+    }
+
     protected void deposit(Double amountDeposited) {
         System.out.println("Enter the amount you wish to deposit: ");
         amountDeposited = scan.nextDouble();
-        if (deposit > 0) {
+        if (amountDeposited > 0) {
             deposit = amountDeposited;
-            // if (balance == null) {
-            //     balance = deposit;
-            // }else{
-            //     balance  = balance + deposit;
-            // }
-             balance  = balance + deposit;
+            balance = balance + deposit;
             transaction = amountDeposited;
             System.out.println("====================");
             System.out.println(name + ": " + " You deposited £" + deposit);
+            accountHolding();
             System.out.println("====================");
         } else {
             System.out.println("Invalid");
@@ -48,28 +52,28 @@ public class BankingSystem {
             System.out.println("Invalid");
             System.out.println("Please enter a valid amount that is higher than 0");
             System.exit(0);
+        } else if (balance == 0.0 || balance == null) {
+            System.out.println("Your account is empty. Please make a deposit");
+            System.exit(0);
         } else {
             withdraw = amountWithdrawn;
             balance = balance - withdraw;
             transaction = amountWithdrawn;
             System.out.println("====================");
             System.out.println(name + ": " + "You withdrew £" + withdraw);
+            accountHolding();
             System.out.println("====================");
         }
 
-        // if (balance == null) {
-        // System.out.println("Your account is empty. Please make a deposit");
-        // System.exit(0);
-        // }
     }
 
-    protected String getTransaction() {
-        return name + ": " + transaction;
+    protected void getTransaction() {
+        System.out.println(name + ": " + transaction);
     }
 
     protected void menu() {
         do {
-            System.out.println("Pick an option between 'deposit, withdraw, transaction'");
+            System.out.println("Pick an option between 'deposit, withdraw, transaction' or enter '0' to exit");
             option = scan.next();
 
             switch (option) {
@@ -86,16 +90,15 @@ public class BankingSystem {
                         System.out.println("====================");
                     } else {
                         System.out.println("====================");
-                        System.out.println("You have made no transactions");
+                        System.out.println("You have made no transaction");
                         System.out.println("====================");
                     }
-
                     break;
                 case "0":
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Pick an option between 'transaction, withdraw, deposit'");
+                    System.out.println("Pick an option between 'deposit, withdraw, transaction' or enter '0' to exit");
 
             }
         } while (true);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Scanner sc = new Scanner(System.in);
         BankingSystem newUser = new BankingSystem();
         newUser.signin("Tosimi", "01");
          newUser.menu();
